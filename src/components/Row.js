1a17eb23a43512a42f1420a771c92a2062e4877a
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-04 02:48:26 -0500

    added data for Rows from api

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,40 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import axios from "../axios.js"
 
-const Row = () => {
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+	const [movies, setMovies] = useState([])
+
+	const base_url = "https://imag.tmdb.org/t/p/original/"
+
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchUrl)
+			setMovies(request.data.results)
+			return request
+		}
+		fetchData()
+	}, [fetchUrl])
+
 	return (
-		<div>
-			<h1>I am a ROW</h1>
-		</div>
+		<RowWrapper>
+			<TitleContainer>{title}</TitleContainer>
+			{/* {movies.map((movie) => (
+				<RowPoster ${isLargeRow && RowLargePoster}
+					src={`${base_url}${
+						isLargeRow ? movie.poster_path : movie.backdrop_path
+					}`}
+					alt={movie.name} */}
+			{/* /> ))} */}
+		</RowWrapper>
 	)
 }
+
+const RowWrapper = styled.div`
+	color: white;
+`
+const TitleContainer = styled.h2``
+
+const MovieImage = styled.img``
 
 export default Row
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,30 @@
 import React from "react"
+import styled from "styled-components"
 import Banner from "../components/Banner"
 import Nav from "../components/Nav"
 import Row from "../components/Row"
+import requests from "../requests"
 
 const Home = () => {
 	return (
-		<>
+		<HomeWrapper>
 			<Nav />
 			<Banner />
-			<Row />
-		</>
+			<Row
+				title='FLUTUREFLIX ORIGINALS'
+				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow
+			/>
+			<Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+			<Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+			<Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+			<Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+			<Row title='Horrow Movies' fetchUrl={requests.fetchHorrorMovies} />
+			<Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+			<Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+		</HomeWrapper>
 	)
 }
+const HomeWrapper = styled.div``
 
 export default Home
