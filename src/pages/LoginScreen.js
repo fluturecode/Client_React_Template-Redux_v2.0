93c8e6198ed4df935dbbd8f9eb98be9b5d7f1c34
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-07 23:52:30 -0500

    added LoginScreen.js

---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+function LoginScreen() {
+	return (
+		<LoginContainer>
+			<LoginBackground />
+		</LoginContainer>
+	)
+}
+
+const LoginContainer = styled.div``
+
+const LoginBackground = styled.img``
+
+export default LoginScreen
