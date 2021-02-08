eb7ed4fd7cea4cd25c967b0a515d0cd178697a60
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-08 18:37:24 -0500

    added image to h LoginScreen.js

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -4,7 +4,10 @@ import styled from "styled-components"
 function LoginScreen() {
 	return (
 		<LoginContainer>
-			<LoginBackground />
+			<LoginBackground
+				src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+				alt='background image'
+			/>
 		</LoginContainer>
 	)
 }
