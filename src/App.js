8e2dc36eee5b22ff04fc57ac4a8c7bf33e9c3e35
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-05 21:54:39 -0500

    added LoginPage.js & public cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import styled from "styled-components"
 import Home from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
 
 const App = () => {
 	return (
 		<AppWrapper>
 			<Router>
+				(!user ? (
+				<LoginPage />
+				): (
 				<Switch>
 					<Route exact path='/'>
 						<Home />
 					</Route>
 				</Switch>
+				))
 			</Router>
 		</AppWrapper>
 	)
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,30 @@
 import React from "react"
 import styled from "styled-components"
 
-function LoginScreen() {
+function LoginPage() {
 	return (
 		<LoginContainer>
 			<LoginBackground
 				src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
 				alt='background image'
 			/>
+			<Button>Sign In</Button>
 		</LoginContainer>
 	)
 }
 
-const LoginContainer = styled.div``
+const LoginContainer = styled.div`
+	position: relative;
+	height: 100%;
+`
 
 const LoginBackground = styled.img``
 
-export default LoginScreen
+const Button = styled.button`
+	position: fixed;
+	right: 20px;
+	top: 20px;
+	padding: 10px 20px;
+`
+
+export default LoginPage
