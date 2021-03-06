4689969a089ff0cef8d53ee8b747d5667d0f53cc
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-05 22:23:08 -0500

    added styling to LoginPage.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import styled from "styled-components"
-import Home from "./pages/HomePage"
+import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 
 const App = () => {
 	return (
-		<AppWrapper>
+		<>
 			<Router>
-				(!user ? (
 				<LoginPage />
-				): (
 				<Switch>
 					<Route exact path='/'>
-						<Home />
+						<HomePage />
 					</Route>
 				</Switch>
-				))
 			</Router>
-		</AppWrapper>
+		</>
 	)
 }
 
-const AppWrapper = styled.div`
-	background: #111;
-`
 export default App
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,11 +4,17 @@ import styled from "styled-components"
 function LoginPage() {
 	return (
 		<LoginContainer>
-			<LoginBackground
+			<LoginLogo
 				src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
-				alt='background image'
+				alt='flutureflix logo'
 			/>
-			<Button>Sign In</Button>
+			<LoginButton>Sign In</LoginButton>
+			<LoginScreenGradient />
+			<LoginScreenBody>
+				<>
+					<h1>Unlimited films, TV programs and more.</h1>
+				</>
+			</LoginScreenBody>
 		</LoginContainer>
 	)
 }
@@ -16,15 +22,43 @@ function LoginPage() {
 const LoginContainer = styled.div`
 	position: relative;
 	height: 100%;
+	background: url("https://assets.nflxext.com/ffe/siteui/vlv3/757ab38f-5d08-40bc-b3eb-eaba63ed8203/93c34f94-56c8-40a7-8b2e-b4aac6427977/GB-en-20210125-popsignuptwoweeks-perspective_alpha_website_medium.jpg")
+		center no-repeat;
+	background-size: cover;
+`
+const LoginLogo = styled.img`
+	position: fixed;
+	left: 0;
+	width: 150px;
+	object-fit: contain;
+	padding-left: 20px;
 `
 
-const LoginBackground = styled.img``
-
-const Button = styled.button`
+const LoginButton = styled.button`
 	position: fixed;
 	right: 20px;
 	top: 20px;
 	padding: 10px 20px;
+	font-size: 1rem;
+	color: #fff;
+	background-color: #e50914;
+	font-weight: 600;
+	border: none;
+	cursor: pointer;
 `
+const LoginScreenGradient = styled.div`
+	width: 100%;
+	z-index: 1;
+	height: 100vh;
+	background: rgba(0, 0, 0, 0.4);
+	background-image: linear-gradient(
+		to top,
+		rgba(0, 0, 0, 0.8) 0,
+		rgba(0, 0, 0, 0) 60%,
+		rgba(0, 0, 0, 0.8) 100%
+	);
+`
+
+const LoginScreenBody = styled.div``
 
 export default LoginPage
